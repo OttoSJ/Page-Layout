f5e4555bb96a886fa3a9ed4d7e33ff9664f97f7b
Otto Jones <ottosjonesjr@gmail.com>
2021-11-13 01:34:54 +0300

    added menu items and pictures to menu page

---
+++ index.js
@@ -0,0 +1,47 @@
+
+let data = [
+    {
+
+        name: "Chicken Satay",
+        price: 7.95,
+        
+    },
+    {
+
+        name: "Fresh Rolls",
+        price: 6.95,
+    },
+   
+]
+// let table = document.get
+// let dataColumnCount = data[0].length;
+
+// let thCell = menu.insertRow(-1);
+// for (let i = 0; i < dataColumnCount; i++) {
+    //     let headerCell = document.createElement("TH");
+    //     headerCell.innerHTML = data[0][i];
+    //     thCell.appendChild(headerCell);
+    //     headerCell.style.border = "1px solid grey"
+    // }
+    
+
+
+
+// let menu = document.getElementById("appetizersSelection");
+// for (let i = 0; i < data.length; i++) {
+//     let name = data[i].name
+//     let price = data[i].price
+
+//     let row = menu.insertRow(i)
+//     let cell1 = row.insertCell(0)
+//     let cell2 = row.insertCell(1)
+    
+//     cell1.innerHTML = name
+//     cell2.innerHTML = price
+//     }
+
+    
+{/* <table>
+      <thead id="appetizers"></thead>
+      <tbody id="appetizersSelection"></tbody>
+    </table> */}
